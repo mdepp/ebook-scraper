14bba31e41be4c644a6f19f08fcbad3cd06d38d3
rain <31737634+mdepp@users.noreply.github.com>
2023-09-03 16:37:08 -0600

    Add phrack support

--- ebook-scraper.go
+++ ebook-scraper.go
@@ -3,11 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/url"
 	"os"
 	"strings"
 
 	"github.com/bmaupin/go-epub"
 	"github.com/gocolly/colly"
+	"github.com/schollz/progressbar/v3"
 )
 
 type TOCEntry struct {
@@ -26,15 +28,90 @@ type Metadata struct {
 	Description string
 }
 
-func childHTML(e *colly.HTMLElement, goquerySelector string) string {
-	text, err := e.DOM.Find(goquerySelector).Html()
-	if err != nil {
-		return ""
-	}
-	return text
+type ScrapedBook struct {
+	meta     Metadata
+	toc      []TOCEntry
+	chapters map[string]Chapter
 }
 
-func scrapeRoyalRoad(baseCollector *colly.Collector, baseURL string) (*epub.Epub, error) {
+type Scraper = func(*colly.Collector, string) (ScrapedBook, error)
+
+func assembleEpub(book ScrapedBook) (*epub.Epub, error) {
+	doc := epub.NewEpub(book.meta.Title)
+	doc.SetAuthor(book.meta.Author)
+	coverImage, err := doc.AddImage(book.meta.CoverURL, "cover")
+	if err != nil {
+		return nil, err
+	}
+	coverCSS, err := doc.AddCSS("assets/cover.css", "")
+	if err != nil {
+		return nil, err
+	}
+	doc.SetCover(coverImage, coverCSS)
+	doc.SetDescription(book.meta.Description)
+
+	bar := progressbar.Default(int64(len(book.toc)), "Assemble epub")
+	for _, tocEntry := range book.toc {
+		bar.Add(1)
+		chapter := book.chapters[tocEntry.URL]
+		_, err := doc.AddSection(chapter.Content, chapter.Title, "", "")
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return doc, nil
+}
+
+func main() {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s <URL>\n", os.Args[0])
+	}
+	flag.Parse()
+	if flag.NArg() < 1 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	baseURL := flag.Arg(0)
+
+	handlers := map[string]Scraper{
+		"www.royalroad.com": scrapeRoyalRoad,
+		"phrack.org":        scrapePhrack,
+	}
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		fmt.Println("Invalid url:", err)
+		os.Exit(1)
+	}
+	handler, ok := handlers[parsedURL.Host]
+	if !ok {
+		fmt.Println("No handler available for url")
+		os.Exit(1)
+	}
+
+	baseCollector := colly.NewCollector(
+		colly.CacheDir(".cache"),
+		colly.AllowedDomains(parsedURL.Host),
+		func(col *colly.Collector) {
+			col.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 5})
+		},
+	)
+
+	scrapedBook, err := handler(baseCollector, baseURL)
+	if err != nil {
+		fmt.Println("Scraping failed: {}", err)
+	}
+	doc, err := assembleEpub(scrapedBook)
+	if err != nil {
+		fmt.Println("Assembling failed: {}", err)
+	}
+
+	filename := strings.ToLower(strings.ReplaceAll(doc.Title(), " ", "-")) + ".epub"
+	doc.Write(filename)
+	fmt.Println("Wrote to", filename)
+}
+
+func scrapeRoyalRoad(baseCollector *colly.Collector, baseURL string) (ScrapedBook, error) {
 	var meta Metadata
 	var toc []TOCEntry
 	var chapters = make(map[string]Chapter)
@@ -42,13 +119,8 @@ func scrapeRoyalRoad(baseCollector *colly.Collector, baseURL string) (*epub.Epub
 	mainCollector := baseCollector.Clone()
 	chapterCollector := mainCollector.Clone()
 
-	mainCollector.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
-
-	chapterCollector.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
+	logVisits(mainCollector)
+	logVisits(chapterCollector)
 
 	mainCollector.OnHTML("html", func(e *colly.HTMLElement) {
 		coverURL := e.ChildAttr(".fic-header img[data-type=\"cover\"]", "src")
@@ -70,64 +142,61 @@ func scrapeRoyalRoad(baseCollector *colly.Collector, baseURL string) (*epub.Epub
 
 	chapterCollector.OnHTML("html", func(e *colly.HTMLElement) {
 		chapterURL := e.Request.URL.String()
+		chapterTitle := e.ChildText(".fic-header h1")
+		chapterContent := "<h2>" + chapterTitle + "</h2>" + childHTML(e, ".chapter-content")
 		chapters[chapterURL] = Chapter{
-			Title:   e.ChildText(".fic-header h1"),
-			Content: childHTML(e, ".chapter-content"),
+			Title:   chapterTitle,
+			Content: chapterContent,
 		}
 	})
 
-	mainCollector.Visit(baseURL)
-
-	doc := epub.NewEpub(meta.Title)
-	doc.SetAuthor(meta.Author)
-	coverImage, err := doc.AddImage(meta.CoverURL, "cover")
+	err := mainCollector.Visit(baseURL)
 	if err != nil {
-		return nil, err
+		return ScrapedBook{}, err
 	}
-	coverCSS, err := doc.AddCSS("assets/cover.css", "")
-	if err != nil {
-		return nil, err
-	}
-	doc.SetCover(coverImage, coverCSS)
-	doc.SetDescription(meta.Description)
-
-	for _, tocEntry := range toc {
-		chapter := chapters[tocEntry.URL]
-		chapterPrelude := "<h2>" + chapter.Title + "</h2>"
-		_, err := doc.AddSection(chapterPrelude+chapter.Content, chapter.Title, "", "")
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return doc, nil
+	return ScrapedBook{meta, toc, chapters}, nil
 }
 
-func main() {
-	flag.Usage = func() {
-		fmt.Println("Usage: ./ebook-scraper <URL>")
+func scrapePhrack(baseCollector *colly.Collector, baseURL string) (ScrapedBook, error) {
+	meta := Metadata{
+		Title: "Phrack Magazine", CoverURL: "http://phrack.org/images/phrack-logo.jpg",
 	}
-	flag.Parse()
-	if flag.NArg() < 1 {
-		flag.Usage()
-		os.Exit(1)
-	}
-	baseURL := flag.Arg(0)
+	var toc []TOCEntry
+	var chapters = make(map[string]Chapter)
 
-	baseCollector := colly.NewCollector(
-		colly.CacheDir(".cache"),
-		colly.AllowedDomains("www.royalroad.com"),
-		func(col *colly.Collector) {
-			col.Limit(&colly.LimitRule{DomainGlob: "*", Parallelism: 5})
-		},
-	)
-
-	doc, err := scrapeRoyalRoad(baseCollector, baseURL)
+	logVisits(baseCollector)
+	baseCollector.OnHTML(".details a", func(e *colly.HTMLElement) {
+		childURL := e.Request.AbsoluteURL(e.Attr("href"))
+		baseCollector.Visit(childURL)
+	})
+	baseCollector.OnHTML(".tissue a", func(e *colly.HTMLElement) {
+		childURL := e.Request.AbsoluteURL(e.Attr("href"))
+		toc = append(toc, TOCEntry{URL: childURL})
+		baseCollector.Visit(childURL)
+	})
+	baseCollector.OnHTML("body", func(e *colly.HTMLElement) {
+		chapterURL := e.Request.URL.String()
+		chapterTitle := e.ChildText(".p-title")
+		chapterContent := "<pre>" + e.ChildText("pre") + "</pre>"
+		chapters[chapterURL] = Chapter{Title: chapterTitle, Content: chapterContent}
+	})
+	err := baseCollector.Visit(baseURL)
 	if err != nil {
-		fmt.Println("Scraping failed: {}", err)
+		return ScrapedBook{}, err
 	}
+	return ScrapedBook{meta, toc, chapters}, nil
+}
 
-	filename := strings.ToLower(strings.ReplaceAll(doc.Title(), " ", "-")) + ".epub"
-	doc.Write(filename)
-	fmt.Println("Wrote to", filename)
+func logVisits(collector *colly.Collector) {
+	collector.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+}
+
+func childHTML(e *colly.HTMLElement, goquerySelector string) string {
+	text, err := e.DOM.Find(goquerySelector).Html()
+	if err != nil {
+		return ""
+	}
+	return text
 }
--- go.mod
+++ go.mod
@@ -17,10 +17,16 @@ require (
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.3.1 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/schollz/progressbar/v3 v3.13.1 // indirect
 	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/vincent-petithory/dataurl v0.0.0-20191104211930-d1553a71de50 // indirect
 	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/term v0.12.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 )
